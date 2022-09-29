9aa66d112c4f8fd46020cbc7d9e940aa77a66f43
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-09-29 23:35:27 +0300

    commit tree 73e76e3af67921a054f8061677e527f213a089dc

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,17 @@
 class MoviesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-
   def index
     movies = Movie.all
     render json: movies
   end
-
   def show
     movie = Movie.find(params[:id])
     render json: movie
+  end
+
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
   end
 
   private
